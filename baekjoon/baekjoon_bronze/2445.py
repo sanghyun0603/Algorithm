ca1474c1d94e1d525ef57ffc71dbdc21c875a059
배상현 <tkdgusdl63@naver.com>
2022-08-11 08:54:44 +0900

    baekjoon

---
+++ baekjoon/baekjoon_bronze/2445.py
@@ -0,0 +1,14 @@
+N = int(input())
+for i in range(N):
+    print('*'*(i+1),end='')
+    print(' '*((2*N)-(2*(i+1))),end='')
+    print('*'*(i+1), end='')
+    print()
+for i in range(N-1,0,-1):
+    print(('*'*i),end='')
+    print(' '*((2*N)-(i*2)),end='')
+    print('*'*i,end='')
+    print()
+
+# 8 6 4 2 0
+# 2 4 6 8
--- baekjoon/baekjoon_bronze/5532.py
+++ baekjoon/baekjoon_bronze/5532.py
@@ -8,6 +8,6 @@ sty = korea/ko_pg
 sty_m = mathm/ma_pg
 va = max(sty,sty_m)
 if va - math.trunc(va) == 0 :
-    print(vac - va)
+    print(int(vac - va))
 else:
     print(int(vac - (math.trunc(va))-1))
