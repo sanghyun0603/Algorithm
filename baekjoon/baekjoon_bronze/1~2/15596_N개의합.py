3fb7615aa3dc98c417dcc87eafcbfe857bd04c26
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2022-08-12 17:55:53 +0900

    0812study

---
+++ baekjoon/baekjoon_bronze/1~2/15596_N개의합.py
@@ -0,0 +1,5 @@
+def solve(a):
+    return sum(a)
+
+b = [1,2,3,4,5]
+print(solve(b))
---
+++ baekjoon/baekjoon_bronze/class1_easy/10871.py
@@ -0,0 +1,11 @@
+N ,X = map(int,input().split())
+num_list = list(map(int,input().split()))
+for i in num_list:
+    if i < X :
+        print(i,end=' ')
+# s1 = 'abc'
+# s2 = 'abc'
+# s3 = 'def'
+# s4 = s1
+# s5 = s1[:2]+'c'
+# print(s1 is s5)
